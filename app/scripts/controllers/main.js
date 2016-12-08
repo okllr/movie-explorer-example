735d095d990565d54d98d13e5518824929df357d
Olivier Keller <olivierkeller@live.fr>
2016-12-08 11:45:46 +0100

    Tuto part 3.5

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,4 +1,6 @@
-'use strict';
+ 'use strict';
+
+// jscs:disable requireCamelCaseOrUpperCaseIdentifiers
 
 /**
  * @ngdoc function
@@ -12,9 +14,70 @@ angular.module('movieExplorerApp')
     var ctrl = this;
 
     ctrl.movies = [];
+    ctrl.slides = [];
 
     movies.getDiscoverMovies().then(function(result)
     {
       ctrl.movies = result;
-    });
-  });
+
+	    movies.getConfiguration().then(function(result){
+	    	ctrl.configuration = result;
+	    }).then(function(){
+	    	angular.forEach(ctrl.movies, function(movie){
+	    		if(movie.backdrop_path){
+	      			ctrl.slides.push(ctrl.getImageURL(movie.backdrop_path,'w780'));
+	      		}
+	      	});
+	      	ctrl.currentSlide = 0;
+	    });
+	});
+
+    ctrl.getImageURL = function(image, size){
+      if(ctrl.configuration)
+      {
+        return ctrl.configuration.images.secure_base_url + size + image;
+      }else{
+        return null;
+      }
+    };
+
+    ctrl.getNextSlideIndex = function(){
+      if(ctrl.currentSlide <= ctrl.slides.length - 1)
+      {
+        return 0;
+      }else{
+        return ctrl.currentSlide + 1;
+      }
+    };
+
+    ctrl.getPrevSlideIndex = function(){
+      if(ctrl.currentSlide === 0)
+      {
+        return ctrl.slides.length - 1;
+      }else{
+        return ctrl.currentSlide - 1;
+      } 
+    };
+
+   	ctrl.slideIsVisible = function(index){
+    	return index === ctrl.currentSlide || index === ctrl.getPrevSlideIndex() || index === ctrl.getNextSlideIndex();   
+    };
+
+    ctrl.getSlideClass = function(index){
+    	if(index === ctrl.currentSlide){
+    		return 'current';
+    	} else if(index === ctrl.getPrevSlideIndex()){
+    		return 'previous';
+    	} else if(index === ctrl.getNextSlideIndex()){
+    		return 'next';
+    	}
+    	return '';
+    };
+
+    ctrl.gotoSlide = function(index){
+    	ctrl.currentSlide = index;
+    };
+
+});
+
+// jscs:enable requireCamelCaseOrUpperCaseIdentifiers
